70df54f50cd3e269ee0e02dc8d399516c257a7f4
paulMuema <pkmuema87@gmail.com>
2024-02-04 12:16:28 +0300

    Reads file and posts addresses on console

---
+++ src/main/java/inetaddresses/inetaddresses/InetAddresses.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package inetaddresses.inetaddresses;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+public class InetAddresses {
+
+    public static void main(String[] args) {
+        
+        String filePath = "C:\\Users\\hp\\Desktop\\USIU\\Assignments_and_notes\\Programming with Networks\\test.txt";
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+               resolveAndPrintAddresses(line); 
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+    }
+}
+    
+private static void resolveAndPrintAddresses(String url) {
+    try {
+        InetAddress [] addresses = InetAddress.getAllByName(url);
+        
+        System.out.println("IP Addresses for " + url + ":");
+        for (InetAddress address : addresses) {
+            System.out.println(address.getHostAddress());
+    } 
+        System.out.println();
+} catch (UnknownHostException e) {
+    System.err.println("Unable to resolve host: " + url);
+    e.printStackTrace();
+}
+}
+}
+    
+
+    
